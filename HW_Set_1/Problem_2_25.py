f0bcbb4a654d1d8e539db3c4ecaddc2d2c366d6a
flameofanor <68980551+flameofanor@users.noreply.github.com>
2026-01-27 09:47:30 -0900

    gg

---
+++ HW_Set_1/Problem_2_25.py
@@ -0,0 +1,20 @@
+import math
+from math import sin, cos, tan, asin, acos, atan
+from math import pow, sqrt
+from math import e
+
+#constants
+g = 9.8 #m/s
+G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
+k = 8.988 * 10 ** 9
+
+#problem variables
+
+# p = v**2 / R
+def power_dissapated(volts, ohms):
+	return volts ** 2 / ohms
+
+
+
+
+
