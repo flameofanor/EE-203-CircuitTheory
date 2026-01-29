058c59a2b9274f2246013ee893ac2f54884d8113
flameofanor <68980551+flameofanor@users.noreply.github.com>
2026-01-29 09:24:39 -0900

    gg

---
+++ Class Notes_1_29/dependent_current_nodes.py
@@ -0,0 +1,34 @@
+import math
+from math import sin, cos, tan, asin, acos, atan
+from math import pow, sqrt
+from math import e
+
+#constants
+g = 9.8 #m/s
+G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
+k = 8.988 * 10 ** 9
+
+#problem variables
+r1 = 5
+r2 = 10
+r3 = 20
+r4 = 5
+
+v = 20
+
+i = None
+#iD = 2 * i
+
+v1 = 13.8
+v2 = 8.3
+
+i = (v1 - v2) / r2
+print(i)
+
+
+
+
+
+
+
+
---
+++ Class Notes_1_29/dependent_voltage_nodes.py
@@ -0,0 +1,28 @@
+import math
+from math import sin, cos, tan, asin, acos, atan
+from math import pow, sqrt
+from math import e
+
+#constants
+g = 9.8 #m/s
+G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
+k = 8.988 * 10 ** 9
+
+#problem variables
+
+v1 = 50
+v2 = 0
+v3 = 0
+i = 0
+i_source = 4
+
+i1 = 0
+iD = 10 * i1
+
+
+
+
+
+
+
+
