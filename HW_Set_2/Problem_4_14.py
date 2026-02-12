7f13cc407c30c66a91d80de9d4bfbd15639e3be6
flameofanor <68980551+flameofanor@users.noreply.github.com>
2026-02-11 18:17:11 -0900

    gg

---
+++ HW_Set_2/Problem_4_14.py
@@ -0,0 +1,25 @@
+import math
+from math import sin, cos, tan, asin, acos, atan
+from math import pow, sqrt
+from math import e
+# f"{some_float:.2f}"
+# print("R1 = " + f"{r1:.2f}" + " \u03A9")
+
+#constants
+g = 9.8 #m/s
+G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
+k = 8.988 * 10 ** 9
+
+#problem variables
+
+r1 = 1
+r2 = 2
+r3 = 1
+r4 = 6
+r5 = 12
+r6 = 24
+
+va = 125
+vb = 250
+
+
