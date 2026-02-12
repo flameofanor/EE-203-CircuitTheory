ae4ba4cb75c548a50bf8cc2345b09e6c8e35ff9c
flameofanor <68980551+flameofanor@users.noreply.github.com>
2026-02-12 09:51:07 -0900

    notes on mesh current

---
+++ Class Notes_2_12/MeshCurrent.py
@@ -0,0 +1,21 @@
+import math
+from math import sin, cos, tan, asin, acos, atan
+from math import pow, sqrt
+from math import e
+# f"{some_float:.2f}"
+# print("R1 = " + f"{r1:.2f}" + " \u03A9")
+
+#constants
+g = 9.8 #m/s
+G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
+k = 8.988 * 10 ** 9
+
+#problem variables
+
+
+
+i1 = 2.9
+i2 = -6.16
+
+v = 2 * (i2 + 12) + 14 * (i1 + 12)
+print(v)
