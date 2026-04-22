a3d4ab0d87d9092f9bf4e2c6b95e4b5ff42d0f6b
flameofanor <68980551+flameofanor@users.noreply.github.com>
2026-04-22 08:26:22 -0800

    gg

--- HW_Set_7/Problem_7_24.py
+++ HW_Set_7/Problem_7_24.py
@@ -11,3 +11,23 @@ G = 6.6743 * 10 ** (-11) #m^3 / kg * s^2
 k = 8.988 * 10 ** 9
 
 #problem variables
+
+r1 = 15e3
+r2 = 20e3
+r3 = 40e3
+
+c1 = 5e-6
+c2 = 1e-6
+r_sum = r1 + r2 + r3
+
+V = 15
+
+v1 = (r2 * V) / r_sum
+print(v1)
+
+v2 = (r3 * V) / r_sum
+print(v2)
+
+tau = r2 * c1
+print(1/tau)
+
